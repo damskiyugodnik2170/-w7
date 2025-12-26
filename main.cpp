3e6ada8a394685c5eb4342bd6dd1b9de2a66737e
are you ladiesman 217? <damskiyugodnik981@gmail.com>
2025-12-26 09:21:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
-#include <iostream>  
-#include <string>  
+#include <iostream>
 using namespace std;
 int main() {
-    setlocale(LC_ALL, "");
-    string password = "12345";
-    string userPassword;
-    cout << "\x1b[6m                                                   [+] Вход в систему [+]\x1b[0m\n";
-    bool Correct = false; 
-    while (!Correct) {
-        
-        cout << "[+] Введите пароль: ";
-        cin >> userPassword;
-
-        if (userPassword == password) {
-            cout << "\x1b[92m[+] Добро пожаловать!\x1b[0m" << endl;
-            Correct = true;
-        }
-        else {
-            cout << "\x1b[31m[-] Неверный пароль!\x1b[0m" << endl;
-        }
-    }
-
-    return 0;
+	setlocale(LC_ALL, "");
+    string attempt;
+	string password = "12345";
+	while (true) {
+		cout << "[ + ] Введите пароль: ";
+		cin >> attempt;
+		if (attempt == password) {
+			cout << "\x1b[92mДобро пожаловать!\x1b[0m";
+			return 0;
+		}
+		else {
+			cout << "\x1b[31m [-] Неверный пароль!\x1b[0m\n";
+		}
+	}
 }
+
